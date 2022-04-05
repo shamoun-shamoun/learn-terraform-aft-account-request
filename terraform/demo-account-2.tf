d0f4a14d85b3eff0c1b970a9fa0df2b1f43735ef
Shamoun-Shamoun <r0793623@student.thomasmore.be>
2022-04-05 13:11:00 +0200

    Provision a new account

---
+++ terraform/demo-account-2.tf
@@ -0,0 +1,27 @@
+module "sandbox" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+ AccountEmail              = "shamoun.shamoun+demo-account2@ordina.be"
+ AccountName               = "Demo2"
+ ManagedOrganizationalUnit = "Sandbox"
+ SSOUserEmail              = "shamoun.shamoun+demo-account2@ordina.be"
+ SSOUserFirstName          = "Demo2"
+ SSOUserLastName           = "Account2"
+}
+
+account_tags = {
+ "Groep"="BPS"
+}
+
+change_management_parameters = {
+ change_requested_by = "Ordina"
+ change_reason       = "Provisiom a new account"
+}
+
+custom_fields = {
+ group = "Demo-accounts"
+}
+
+account_customizations_name = "demo-account-2"
+}
