f60f0ca3bffd08c21c867377361316ecedd0563c
Shamoun-Shamoun <r0793623@student.thomasmore.be>
2022-04-12 15:47:54 +0200

    dsadasdqweqe

---
+++ terraform/testqwe.tf
@@ -0,0 +1,31 @@
+module "testqwe" {
+    source = "./modules/aft-account-request"
+
+    control_tower_parameters = {
+      AccountEmail              = "asf"
+      AccountName               = "qwe"
+      ManagedOrganizationalUnit = "ewqe"
+      SSOUserEmail              = "rqwr"
+      SSOUserFirstName          = "qwr"
+      SSOUserLastName           = "tet"
+    } 
+
+
+    account_tags = {
+       "eqtqw" ="we"
+    } 
+
+
+    change_management_parameters = {
+       change_requested_by = "qw"
+       change_reason       = "te"
+    } 
+
+
+    custom_fields = {
+       group = "weqr"
+     } 
+
+
+    account_customizations_name = "wqeq"
+    }
