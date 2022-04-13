a8fddea3b9645dfdc29b6011f4ba8d8d68f21d6f
www-data <www-data@ip-172-31-39-102>
2022-04-13 10:12:54 +0000

    dsdsa

---
+++ terraform/hallohallo.tf
@@ -0,0 +1,31 @@
+module "hallohallo" {
+    source = "./modules/aft-account-request"
+
+    control_tower_parameters = {
+      AccountEmail              = "dfsafwq"
+      AccountName               = "fgd"
+      ManagedOrganizationalUnit = "gfwe"
+      SSOUserEmail              = "few"
+      SSOUserFirstName          = "fwe"
+      SSOUserLastName           = "gwe"
+    } 
+
+
+    account_tags = {
+       "wg" ="fwe"
+    } 
+
+
+    change_management_parameters = {
+       change_requested_by = "rweq"
+       change_reason       = "rrwgt"
+    } 
+
+
+    custom_fields = {
+       group = "rewr"
+     } 
+
+
+    account_customizations_name = "rewrw"
+    }
