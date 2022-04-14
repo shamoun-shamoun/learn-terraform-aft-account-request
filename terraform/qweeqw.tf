f25eb5418e9390ebd59b19fb4f474610ad608ab9
www-data <www-data@ip-172-31-39-102.eu-west-1.compute.internal>
2022-04-14 12:39:15 +0000

    scripttest

---
+++ terraform/qweeqw.tf
@@ -0,0 +1,31 @@
+module "qweeqw" {
+    source = "./modules/aft-account-request"
+
+    control_tower_parameters = {
+      AccountEmail              = "eqw"
+      AccountName               = "eq"
+      ManagedOrganizationalUnit = "ewq"
+      SSOUserEmail              = "ewqe"
+      SSOUserFirstName          = "etew"
+      SSOUserLastName           = "gtg"
+    } 
+
+
+    account_tags = {
+       "retr" ="ht"
+    } 
+
+
+    change_management_parameters = {
+       change_requested_by = "sf"
+       change_reason       = "ujytu"
+    } 
+
+
+    custom_fields = {
+       group = "uyiuy"
+     } 
+
+
+    account_customizations_name = "bcvb"
+    }
