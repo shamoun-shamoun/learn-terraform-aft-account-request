a49426b631cf82daee1606170cc6432c9ad65190
ubuntu <ubuntu@ip-172-31-39-102>
2022-04-14 13:11:22 +0000

    ewqeq

---
+++ terraform/ewewewe.tf
@@ -0,0 +1,31 @@
+module "ewewewe" {
+    source = "./modules/aft-account-request"
+
+    control_tower_parameters = {
+      AccountEmail              = "asf"
+      AccountName               = "r"
+      ManagedOrganizationalUnit = "qrq"
+      SSOUserEmail              = "wrw"
+      SSOUserFirstName          = "qr"
+      SSOUserLastName           = "qwr"
+    } 
+
+
+    account_tags = {
+       "wqrwqr" ="qwr"
+    } 
+
+
+    change_management_parameters = {
+       change_requested_by = "qwr"
+       change_reason       = "wrqw"
+    } 
+
+
+    custom_fields = {
+       group = "qwr"
+     } 
+
+
+    account_customizations_name = "rqw"
+    }
