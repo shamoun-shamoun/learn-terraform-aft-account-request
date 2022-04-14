ee8be63dd3abf244dfa0bcd53443822ec329cf71
www-data <www-data@ip-172-31-36-155>
2022-04-14 09:32:46 +0000

    Commit

---
+++ terraform/rwewq.tf
@@ -0,0 +1,31 @@
+module "rwewq" {
+    source = "./modules/aft-account-request"
+
+    control_tower_parameters = {
+      AccountEmail              = "e"
+      AccountName               = "wqeqw"
+      ManagedOrganizationalUnit = "eqw"
+      SSOUserEmail              = "e"
+      SSOUserFirstName          = "qe"
+      SSOUserLastName           = "ewqe"
+    } 
+
+
+    account_tags = {
+       "e" ="q"
+    } 
+
+
+    change_management_parameters = {
+       change_requested_by = "wq"
+       change_reason       = "qwe"
+    } 
+
+
+    custom_fields = {
+       group = "qwe"
+     } 
+
+
+    account_customizations_name = "qwe"
+    }
